d067ed30f8a0491c3102730fad7f016745844ddf
Abhinav <abhinavakarthikeya01@gmail.com>
2026-02-11 19:53:19 +0530

    chnged the display of month cards and added week cards too

--- src/things/realpage.jsx
+++ src/things/realpage.jsx
@@ -7,7 +7,7 @@ import MoodCard from './moodCard.jsx'
 import useLocalStorage from '../hooks/useLocalStorage.js'
 import InfoPage from './infopage.jsx'
 import logo from '../assets/logo.svg';
-
+import Week from './week.jsx'
 
 const RealPage = () => {
   const [items, setItems] = useLocalStorage('mooditems', [
@@ -18,7 +18,7 @@ const RealPage = () => {
   ]);
 
 
-  const [totalMoods,setTotalMoods] = useLocalStorage('totalcount',0)
+  const [totalMoods, setTotalMoods] = useLocalStorage('totalcount', 0)
   const updateTotal = (amount) => {
     setTotalMoods(prev => prev + amount);
   };
@@ -39,7 +39,7 @@ const RealPage = () => {
       >
 
         {/* bg-black/20 */}
-        <div data-theme={aqua ? "aqua" : "retro"} className="font-['Kode Mono'] min-h-screen ">
+        <div data-theme={aqua ? "aqua" : "retro"} className="font-['Sour Gummy'] min-h-screen ">
 
           <div className="h-14 bg-base-300/35 border-b-[1px] border-gray-400 z-10 sticky backdrop-blur-[7px] top-0 w-full flex items-center justify-between">
             <img
@@ -53,7 +53,7 @@ const RealPage = () => {
                   className="btn btn-ghost bg-base-300/70 mr-4"> Dev log
                 </button>
               </Link>
-             
+
 
 
               <label
@@ -68,30 +68,46 @@ const RealPage = () => {
 
             </div>
           </div>
-          <div className="text-left">
-            <InfoPage />
-          </div>
 
+          <div className="flex flex-row max-md:flex-col ">
 
-          <button className="">Total days selected = { totalMoods }</button>
-
-
-
-          <div className=" flex md:flex-row  flex-col">
-           
-            {/* ////////////////////////////  for moods card ///////////////////////////// */}
-            <div className="h-fit " >
+            <div className=" h-fit " >
               <MoodCard items={items} setItems={setItems} aqua={aqua} />
             </div>
 
-            {/* /////////////////////  12 month cards /////////////////// */}
-            <div className=" h-full ">
-
-
-
-              <Month items={items} updateTotal={updateTotal} aqua={aqua} />
+            <div className="text-left">
+              <InfoPage />
             </div>
+          </div>
 
+          {/* <button className="">Total days selected = {totalMoods}</button> */}
+
+
+
+          <div className=" flex md:flex-row mt-10 md:p-10 flex-col">
+
+            {/* ////////////////////////////  for moods card ///////////////////////////// */}
+
+
+            <div className="flex flex-col">
+              {/* /////////////////////  12 month cards /////////////////// */}
+              <div className=" border-t-4 md:ml-20 md:w-[90vw] border-gray-500 bg-base-300 h-full ">
+
+
+               <div className="text-center font-bold text-3xl">Months</div>
+                <Month items={items} updateTotal={updateTotal} aqua={aqua} />
+              </div>
+
+              {/* /////////////////////  week cards /////////////////// */}
+
+              <div className=" border-t-4 mt-10  md:w-[90vw] md:ml-20 mb-16 border-gray-500 bg-base-300 h-full ">
+
+
+               <div className="text-center font-bold text-3xl">Weeks</div>
+
+                <Week items={items} updateTotal={updateTotal} aqua={aqua} />
+              </div>
+            </div>
           </div>
 
 
