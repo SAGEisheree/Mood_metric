b8b9ec4daa59630c13b30cb5483a1bf5602ad725
Abhinav <abhinavakarthikeya01@gmail.com>
2026-02-16 11:19:44 +0530

    made ready for deploying with render

--- frontend/src/hooks/useCloudStorage.js
+++ frontend/src/hooks/useCloudStorage.js
@@ -1,39 +1,34 @@
-import {useState, useEffect} from 'react';
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import api from '../api/axios';
 
 const useCloudStorage = (key, initialValue) => {
-
-
     const [state, setState] = useState(initialValue);
-    const API_URL = `http://localhost:5000/api/storage`;
 
-    // pulling the data from DB after components load
-
-    useEffect(() =>{
+    useEffect(() => {
         const fetchData = async () => {
             try {
-                const res=await axios.get(`${API_URL}/${key}`);
-                if(res.data !== null) {
-                    setState(res.data)
+                const res = await api.get(`/storage/${key}`);
+                if (res.data !== null) {
+                    setState(res.data);
                 }
             } catch (err) {
-                console.error("Error lodaing from clous:", err);
+                console.error("Error loading from cloud:", err);
             }
-        }   
+        };
         fetchData();
     }, [key]);
-    
-  // syncing localstorage to cloud
-  const setValue = async (value)=>{
-    try {
-        setState(value);
-        await axios.post(API_URL, { key, value });
-    } catch (err) {
-        console.error("error saving to cloud", err);
-    }
-  };
-  return [state, setValue];
 
-}
+    // Syncing to cloud
+    const setValue = async (value) => {
+        try {
+            setState(value);
+            await api.post('/storage', { key, value });
+        } catch (err) {
+            console.error("Error saving to cloud:", err);
+        }
+    };
+
+    return [state, setValue];
+};
 
 export default useCloudStorage;
